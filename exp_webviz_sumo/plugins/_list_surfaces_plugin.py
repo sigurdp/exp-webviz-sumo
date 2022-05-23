1b46e4f0af1238c8ff0fd03532af49347179f012
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2022-05-23 10:22:09 +0200

    Removed exception handling from _getblob_button_clicked()

--- exp_webviz_sumo/plugins/_list_surfaces_plugin.py
+++ exp_webviz_sumo/plugins/_list_surfaces_plugin.py
@@ -65,14 +65,11 @@ class ListSurfacesPlugin(WebvizPluginABC):
         def _connect_button_clicked(_n_clicks: int) -> list:
             print("CALLBACK _connect_button_clicked()")
 
-            print("Connecting...")
-
             children = [html.U(html.B("Connect debug info:"))]
 
             token = None
             if self.use_oauth2:
                 token = flask.session.get("access_token")
-
             print(f"TOKEN={token}")
 
             try:
@@ -81,6 +78,7 @@ class ListSurfacesPlugin(WebvizPluginABC):
                     token=token,
                     interactive=False,
                 )
+                print("got explorer instance")
 
                 all_fields: dict = sumo.get_fields()
                 print("\nall_fields:")
@@ -127,8 +125,6 @@ class ListSurfacesPlugin(WebvizPluginABC):
         def _getblob_button_clicked(_n_clicks: int) -> list:
             print("CALLBACK _getblob_button_clicked()")
 
-            print("Connecting...")
-
             children = [html.U(html.B("GetBlob debug info:"))]
 
             sumo_case_id = "0a4b8f65-2d32-91a9-c244-f54d69ea7bb8"
@@ -136,47 +132,39 @@ class ListSurfacesPlugin(WebvizPluginABC):
             token = None
             if self.use_oauth2:
                 token = flask.session.get("access_token")
+            print(f"TOKEN={token}")
 
-            try:
-                sumo = Explorer(
-                    env="dev",
-                    token=None,
-                    interactive=True,
-                )
+            sumo = Explorer(
+                env="dev",
+                token=None,
+                interactive=False,
+            )
+            print("got explorer instance")
 
-                case = sumo.get_case_by_id(sumo_case_id)
+            case = sumo.get_case_by_id(sumo_case_id)
+            print("got sumo case")
 
-                outstr = f"field={case.field_name}  case={case.case_name}  sumo_id={case.sumo_id}"
-                print(outstr)
-                children.append(html.P(outstr))
+            outstr = f"field={case.field_name}  case={case.case_name}  sumo_id={case.sumo_id}"
+            print(outstr)
+            children.append(html.P(outstr))
 
-                surface_collection = case.get_objects(
-                    "surface",
-                    iteration_ids=[0],
-                    realization_ids=[0],
-                )
+            surface_collection = case.get_objects(
+                "surface",
+                iteration_ids=[0],
+                realization_ids=[0],
+            )
 
-                outstr = f"got {len(surface_collection)} surfaces in surface_collection"
-                print(outstr)
+            outstr = f"got {len(surface_collection)} surfaces in surface_collection"
+            print(outstr)
 
-                surf = surface_collection[0]
+            surf = surface_collection[0]
 
-                outstr = f"surface info: tag_name={surf.tag_name} name={surf.name} realization_id={surf.realization_id}"
-                print(outstr)
+            outstr = f"surface info: tag_name={surf.tag_name} name={surf.name} realization_id={surf.realization_id}"
+            print(outstr)
 
-                blob_bytes: bytes = surf.blob
-                byte_stream = BytesIO(blob_bytes)
+            blob_bytes: bytes = surf.blob
+            byte_stream = BytesIO(blob_bytes)
 
-                children.append(html.P("GetBlob done"))
-
-            except Exception as exc:
-                print("Exception occurred:")
-                print("---------------------------------------------------------------")
-                traceback.print_exc()
-                print("---------------------------------------------------------------")
-                return [
-                    html.U(html.B("Exception occurred:")),
-                    html.P(str(exc)),
-                ]
+            children.append(html.P("GetBlob done"))
 
             return children
